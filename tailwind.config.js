7087292f85990764b11ce8306f5af68a3480d194
ANIL <anilkumarbobba26@gmail.com>
2025-01-20 21:48:13 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme:{
+    extend:{
+      screens:{
+        xs:'425px',
+      },
+      boxShadow:{
+        '4xl':'rgba(149, 157, 165, 0.2) 0px 8px 24px'
+      },
+      fontSize:{
+        xsm:'13px',
+        xss:'9px'
+      },
+      colors:{
+        cyan:'#00435d',
+        'c-orange':"#ed8818"
+      },
+      height:{
+        'c-19':'70px'
+      }
+    }
+  }
+}
